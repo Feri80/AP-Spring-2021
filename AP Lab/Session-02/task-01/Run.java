d6635956e191f188984d79262ed61c21618bdb01
Feri80 <bigfarhad7780@gmail.com>
2021-03-13 10:55:14 +0330

    session 3 added

--- AP Lab/Session-02/task-01/Run.java
+++ AP Lab/Session-02/task-01/Run.java
@@ -12,14 +12,25 @@ public class Run
         Lab ap = new Lab(10, "Sat");
         ap.enrollStudent(std1);
         ap.enrollStudent(std2);
-        
         ap.calcAvg();
         System.out.println("\n" + ap.getAvg() + "\n");
+        
+        Student std3 = new Student("Amir", "Hosseini", "0031007");
+        Student std4 = new Student("Mohammad", "Hasani", "0031008");
+        std3.setGrade(16.50);
+        std4.setGrade(15.75);
+
+        Lab lc = new Lab(10, "Sat");
+        lc.enrollStudent(std3);
+        lc.enrollStudent(std4);
+        lc.calcAvg();
+        System.out.println("\n" + lc.getAvg() + "\n");
 
 
         Department ce = new Department("CE", 10);
 
         ce.addLab(ap);
+        ce.addLab(lc);
 
         ce.print();
     }
